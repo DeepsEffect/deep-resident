a7518559a6da52688874ffbb056aefa91129cc78
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-08 07:50:54 +0600

    deepresident project setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+const Main = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
