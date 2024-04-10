b6433bb15592b61ac25b8220ee60c91cdb79ae9e
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-04-10 08:34:25 +0600

    fixed banner width and added animate css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import "animate.css";
 
 // Import Swiper styles
 import "swiper/css";
@@ -19,16 +20,23 @@ const Banner = () => {
       }}
       navigation={true}
       modules={[Autoplay, Pagination, Navigation]}
-      autoplay={{delay: 3000}}
-      className="mySwiper"
+      autoplay={{ delay: 4000 }}
+      className="mySwiper lg:rounded-xl mt-6 lg:mt-10"
     >
-      <SwiperSlide>
+      <SwiperSlide className="">
         <img
           src={
             "https://images.unsplash.com/photo-1592595896551-12b371d546d5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           }
           alt=""
         />
+        <div className="absolute text-white  bottom-10 left-10 text-left w-1/3 animate__animated animate__delay-5s animate__fadeInDown animate__slower animate__infinite">
+          <h2 className="font-bold text-2xl">Cozy Family Home</h2>
+          <p className="text-base">
+            Discover your perfect family retreat, complete with spacious living
+            areas and a backyard for unforgettable moments.
+          </p>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
         <img
@@ -37,6 +45,13 @@ const Banner = () => {
           }
           alt=""
         />
+        <div className="absolute text-white  bottom-10 left-10 text-left w-1/3 animate__animated animate__delay-5s animate__fadeInDown animate__slower animate__infinite">
+          <h2 className="font-bold text-2xl">Charming Townhouse</h2>
+          <p className="text-base">
+            Experience the allure of townhouse living, blending timeless
+            elegance with contemporary comfort in the heart of the city.
+          </p>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
         <img
@@ -45,6 +60,13 @@ const Banner = () => {
           }
           alt=""
         />
+        <div className="absolute text-white  bottom-10 left-10 text-left w-1/3 animate__animated animate__delay-5s animate__fadeInDown animate__slower animate__infinite">
+          <h2 className="font-bold text-2xl">Vacation Rental Getaway</h2>
+          <p className="text-base">
+            Indulge in the ultimate vacation experience with our stunning rental
+            properties, offering breathtaking views and luxurious amenities.
+          </p>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
